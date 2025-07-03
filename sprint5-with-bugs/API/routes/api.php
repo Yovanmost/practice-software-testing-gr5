9ec8fcff7f5df4483d5150e24472c81e396529f5
Yovanmost <yovanmost@gmail.com>
2025-07-03 22:53:46 +0700

    feat: debugging 30

--- sprint5-with-bugs/API/routes/api.php
+++ sprint5-with-bugs/API/routes/api.php
@@ -25,6 +25,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test-route', function () {
+    return response()->json(['msg' => 'Test route is alive']);
+});
+
 Route::get('/status', function () {
     return response()->json(['version' => config('app.version'), 'environment' => env('APP_ENV'), 'app_name' => env('APP_NAME')], 200,
         ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
